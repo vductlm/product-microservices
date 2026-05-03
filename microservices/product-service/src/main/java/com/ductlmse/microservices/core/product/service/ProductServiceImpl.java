69dd931bdc0c5b70a6baf4248d345e8fb63ca8a2
vductlm <tranleminhduc080699@example.com>
2026-05-03 16:39:26 +0700

    update util and api utils implementation of product service

--- microservices/product-service/src/main/java/com/ductlmse/microservices/core/product/service/ProductServiceImpl.java
+++ microservices/product-service/src/main/java/com/ductlmse/microservices/core/product/service/ProductServiceImpl.java
@@ -1,7 +1,13 @@
 package com.ductlmse.microservices.core.product.service;
 
+import com.ductlmse.microservices.api.product.Product;
+import com.ductlmse.microservices.api.product.ProductService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    @Override
+    public Product getProduct(int productId) {
+        return null;
+    }
 }
